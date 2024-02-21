6d9bc9e66260e06ae0f480babf6138d32ef34357
Nathan Perrier <nathanperrier138@gmail.com>
2024-02-21 04:08:05 +0000

    Update render calls with user authentication information

--- atc_site/backend/atc/subjects.py
+++ atc_site/backend/atc/subjects.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
 def english(request):
-    return render(request, 'atc_site//subjects//english.html')
+    return render(request, 'atc_site//subjects//english.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def maths(request):
-    return render(request, 'atc_site//subjects//maths.html')
+    return render(request, 'atc_site//subjects//maths.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def music(request):
-    return render(request, 'atc_site//subjects//music.html')
+    return render(request, 'atc_site//subjects//music.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def religion(request):
-    return render(request, 'atc_site//subjects//religion.html')
+    return render(request, 'atc_site//subjects//religion.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def science(request):
-    return render(request, 'atc_site//subjects//science.html')
+    return render(request, 'atc_site//subjects//science.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def technology(request):
-    return render(request, 'atc_site//subjects//technology.html')
+    return render(request, 'atc_site//subjects//technology.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
--- atc_site/backend/atc/terms.py
+++ atc_site/backend/atc/terms.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
 def cookie(request):
-    return render(request, 'atc_site//terms and policies//cookie_policy.html')
+    return render(request, 'atc_site//terms and policies//cookie_policy.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def copyright(request):
-    return render(request, 'atc_site//terms and policies//copyright_policy.html')
+    return render(request, 'atc_site//terms and policies//copyright_policy.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def privacy(request):
-    return render(request, 'atc_site//terms and policies//privacy_policy.html')
+    return render(request, 'atc_site//terms and policies//privacy_policy.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def terms_conditions(request):
-    return render(request, 'atc_site//terms and policies//terms_and_conditions.html')
+    return render(request, 'atc_site//terms and policies//terms_and_conditions.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def safety(request):
-    return render(request, 'atc_site//terms and policies//safety_policy.html')
+    return render(request, 'atc_site//terms and policies//safety_policy.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
 
 def terms_of_use(request):
-    return render(request, 'atc_site//terms and policies//terms_of_use.html')
+    return render(request, 'atc_site//terms and policies//terms_of_use.html', {'user': request.user, 'is_authenticated': request.user.is_authenticated})
