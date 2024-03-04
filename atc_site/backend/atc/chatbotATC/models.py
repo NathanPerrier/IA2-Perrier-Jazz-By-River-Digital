a1a5cf6635b670c124b690d72cc2340170f9803a
nathan-perrier23 <nathanperrier138@gmail.com>
2024-03-04 23:33:03 +1000

    Refactor chatbotATC models and views

--- atc_site/backend/atc/chatbotATC/models.py
+++ atc_site/backend/atc/chatbotATC/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from atc_site.models import CustomUser, CustomUserManager
 import hashlib
 
-class MessageATC(models.Model):
+class Message(models.Model):
     role = models.CharField(max_length=200)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     model = models.CharField(max_length=50, blank=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, related_name='chatbotATC_messages')
 
--- atc_site/backend/atc/chatbotATC/views.py
+++ atc_site/backend/atc/chatbotATC/views.py
@@ -6,7 +6,7 @@ import json
 
 from .bot.main import ChatbotATC
 
-from .models import MessageATC
+from .models import Message
 
 
 from .bot.__init__ import GPT_MODEL
@@ -15,11 +15,11 @@ from .bot.__init__ import GPT_MODEL
 @csrf_exempt
 def reset_messages(request):
     # Delete MessageATCs for the current user
-    MessageATC.objects.filter(user=request.user).delete()
+    Message.objects.filter(user=request.user).delete()
 
     # Create a system MessageATC
     try:
-        MessageATC.objects.create(role='system', content='You are a helpful weather assistant that has access to almost all weather data. You are to answer purely weather-based questions. Try to include figures in your response to justify your reasoning.', model=GPT_MODEL, user=request.user)
+        Message.objects.create(role='system', content='You are a helpful weather assistant that has access to almost all weather data. You are to answer purely weather-based questions. Try to include figures in your response to justify your reasoning.', model=GPT_MODEL, user=request.user)
     except Exception as e:
         print('error:', e)
         
@@ -31,9 +31,9 @@ def reset_messages(request):
 def chat(request):
     print(request.user.first_name)
     user_messgae = request.POST.get('MessageATC')
-    MessageATC.objects.create(role='user', content=user_messgae, model=MessageATC.objects.filter(user=request.user).all().order_by('timestamp').last().model, user=request.user)
+    Message.objects.create(role='user', content=user_messgae, model=Message.objects.filter(user=request.user).all().order_by('timestamp').last().model, user=request.user)
     
-    previous_MessageATCs = MessageATC.objects.filter(user=request.user).all().order_by('timestamp')
+    previous_MessageATCs = Message.objects.filter(user=request.user).all().order_by('timestamp')
     if previous_MessageATCs.count() > 0:
         
         formatted_messages = [{'role': msg.role, 'content': msg.content} for msg in previous_MessageATCs]
@@ -44,16 +44,16 @@ def chat(request):
         print('###############################################################################################')
 
         bot_response = ChatbotATC(previous_MessageATCs.last().model).chat_completion_request(formatted_messages)
-        MessageATC.objects.create(role='assistant', content=bot_response, model=previous_MessageATCs.last().model, user=request.user)
+        Message.objects.create(role='assistant', content=bot_response, model=previous_MessageATCs.last().model, user=request.user)
     else:
         bot_response = ChatbotATC(GPT_MODEL).chat_completion_request(request.POST.get('MessageATC'))
-        MessageATC.objects.create(role='assistant', content=bot_response, model=GPT_MODEL, user=request.user)
-    print(MessageATC.objects.all())
+        Message.objects.create(role='assistant', content=bot_response, model=GPT_MODEL, user=request.user)
+    print(Message.objects.all())
     return JsonResponse({'MessageATC': bot_response})
 
 @require_POST
 @csrf_exempt
 def change_model(request):
-    MessageATC.objects.create(role='system', content=f'You are now using the {request.POST.get("model")} model', model=request.POST.get('model'), user=request.user)
+    Message.objects.create(role='system', content=f'You are now using the {request.POST.get("model")} model', model=request.POST.get('model'), user=request.user)
     return JsonResponse({'success': True})
 
