d30362dd72b49f0e97fae6485eee7d0a1311be34
nathan-perrier23 <nathanperrier138@gmail.com>
2024-03-07 09:48:39 +1000

    Delete unnecessary migration files and update login views

--- atc_site/backend/auth/login/views.py
+++ atc_site/backend/auth/login/views.py
@@ -8,24 +8,31 @@ from ...weather_app.main import index as indexWeather
 
 def loginViewWeather(request, error=None):
     if request.method == 'POST':
+        print('login POST')
         user = authenticate(request, email=request.POST['email'], password=request.POST['password']) #CustomUserManager().authenticate(email=request.POST['email'], password=request.POST['password']) 
         if user is not None:
+            print('User authenticated')
             user.backend = 'django.contrib.auth.backends.ModelBackend'
             loginRequest(request, user, backend='django.contrib.auth.backends.ModelBackend')
             # auth_login(request, user)
             request.session['user_id'] = user.id
             return indexWeather(request)
+        print('not authenticated')
         return login_page_weather(request, error='Invalid Login')
     return login_page_weather(request)
 
 def loginViewATC(request, error=None):
     if request.method == 'POST':
+        print('login POST')
+        print(request.POST['email'], request.POST['password'])
         user = authenticate(request, email=request.POST['email'], password=request.POST['password']) #CustomUserManager().authenticate(email=request.POST['email'], password=request.POST['password']) 
         if user is not None:
+            print('User authenticated')
             user.backend = 'django.contrib.auth.backends.ModelBackend'
             loginRequest(request, user, backend='django.contrib.auth.backends.ModelBackend')
             # auth_login(request, user)
             request.session['user_id'] = user.id
             return indexATC(request)
+        print('not authenticated')
         return login_page_atc(request, error='Invalid Login')
     return login_page_atc(request)
