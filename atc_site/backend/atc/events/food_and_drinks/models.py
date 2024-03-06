0d262f848e2ae5dab244b2fa43ce8c9297a802e8
nathan-perrier23 <nathanperrier138@gmail.com>
2024-03-06 13:27:43 +1000

    Update FoodAndDrinksItem model in models.py

--- atc_site/backend/atc/events/food_and_drinks/models.py
+++ atc_site/backend/atc/events/food_and_drinks/models.py
@@ -6,9 +6,9 @@ class FoodAndDrinksItem(models.Model):
     name = models.CharField(max_length=200, blank=False)
     description = models.TextField(blank=False)
     price = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
-    stock = models.IntegerField(min_digitis=0, default=0, blank=False)
-    quantity_sold = models.IntegerField(min_digitis=0, default=0, blank=False)
-    discount = models.FloatField(blank=True)
+    stock = models.IntegerField(default=0, blank=False) #? min
+    quantity_sold = models.IntegerField(default=0, blank=False) #? min
+    discount = models.FloatField(blank=True) #? min
     creation_date = models.DateTimeField(auto_now_add=True, blank=False)
     last_modification = models.DateTimeField(auto_now=True, blank=False)
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
